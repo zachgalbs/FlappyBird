eea889732d867a89776c67291bfd292fdf6327b2
zachgalbs <zacharythomasgalbraith@gmail.com>
2022-08-19 00:42:54 -0700

    half-way to getting the jump fully functional

--- Assets/movementScript.cs
+++ Assets/movementScript.cs
@@ -8,8 +8,10 @@ public class movementScript : MonoBehaviour
     public GameObject pipe;
     public float jumpHeight;
     private float lastTime;
+    private float smoothTime = 0.3F;
     public float creationDelay; 
     private float[] ySlotValues = {-2.5F, 2.5F};
+    private Vector3 targetPosition;
 
     void Start()
     {
@@ -19,16 +21,21 @@ public class movementScript : MonoBehaviour
     void Update()
     {
         // This is saying: whenever w is pressed, move up the object by a certain amount
-        if (Input.GetKey(KeyCode.W)) 
+        if (Input.GetKeyDown(KeyCode.Space)) 
         {
             transform.position += Vector3.up * jumpHeight;
+            // This is for the smooth animation
+            ypos = transform.position.y + Vector3.up * jumpHeight;
+            Vector3 newPos = new Vector3(transform.position.x, (ypos), transform.position.z);
+            targetPosition = newPos;
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref jumpHeight, smoothTime);
         }
+
         // This is the code to make multiple pipes
         if (Time.time - lastTime > creationDelay)
         {
             int upOrDown = Random.Range(0,2);
-            Debug.Log(ySlotValues[upOrDown]);
-            GameObject newpipe = Instantiate(pipe, new Vector3(11F, ySlotValues[upOrDown], 0F), Quaternion.identity);
+            GameObject newpipe = Instantiate(pipe, new Vector2(11F, ySlotValues[upOrDown]), Quaternion.identity);
             lastTime = Time.time;
         }
     }
